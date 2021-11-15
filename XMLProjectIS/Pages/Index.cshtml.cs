3e4a00b27fe5060f71e1f665c0b2e44df6f56d09
Anukriti007 <sharma.anukriti0794@gmail.com>
2021-11-15 18:45:30 -0500

    code for reflecting the Json connection

--- XMLProjectIS/Pages/Index.cshtml.cs
+++ XMLProjectIS/Pages/Index.cshtml.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using QuickType;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace XMLProjectIS.Pages
@@ -19,7 +21,16 @@ namespace XMLProjectIS.Pages
 
         public void OnGet()
         {
-
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://raw.githubusercontent.com/Anukriti007/XMLProjectIS/master/csvjson(2).json");
+                var welcome = Welcome.FromJson(jsonString);
+                ViewData["Welcome"] = welcome;
+                string jsonString1 = webClient.DownloadString("https://raw.githubusercontent.com/Anukriti007/XMLProjectIS/master/csvjson(1).json");
+                var welcome1 = Welcome.FromJson(jsonString1);
+                ViewData["Welcome1"] = welcome1;
+            }
         }
     }
 }
+
