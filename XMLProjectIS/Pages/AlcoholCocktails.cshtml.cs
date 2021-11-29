7ee30aea533d75857c9e81495d30bb55a4362871
Anukriti007 <sharma.anukriti0794@gmail.com>
2021-11-29 18:40:31 -0500

    Modifying the ChicagoData page + Addition of Brewery Arc Page

--- XMLProjectIS/Pages/AlcoholCocktails.cshtml.cs
+++ XMLProjectIS/Pages/AlcoholCocktails.cshtml.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -22,3 +22,4 @@ namespace AlcoholCocktail.Pages
         }
     }
 }
+*/
--- XMLProjectIS/Pages/ChicagoHouses.cshtml.cs
+++ XMLProjectIS/Pages/ChicagoHouses.cshtml.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -24,4 +24,3 @@ namespace ChicagoHousePage.Pages
         }
     }
 }
-*/
