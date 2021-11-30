ca3f941ddfe67b9df8045a958d1e78970009707b
vbdubal <vbdubal.73@gmail.com>
2021-11-29 19:45:51 -0500

    Created Form classes

---
+++ XMLProjectIS/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XMLProjectIS
+{
+    public class Movie
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string MovieName { get; set; }
+        public string TVShow { get; set; }
+
+    }
+}
