f03f4e4e2a2946e1d69f3e7687b577df5a553ecc
vbdubal <vbdubal.73@gmail.com>
2021-11-30 20:51:42 -0500

    Cosmetic changes on Details page

--- XMLProjectIS/Pages/ItemData.cshtml.cs
+++ XMLProjectIS/Pages/ItemData.cshtml.cs
@@ -1,23 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TV;
 
 namespace XMLProjectIS.Pages
 {
     public class ItemDataModel : PageModel
     {
-
         public string MovieIdentifier { get; set; }
         public void OnGet()
         {
+
+
             APICall adapter = new APICall();
 
             var movieDetailsData = adapter.GetMovieDetails();
+            
             MovieIdentifier = Request.Query["MovieIdentifier"];
 
+            //List<MovieDetails.Item> specificMovieSearch = new List<MovieDetails.Item>();
             MovieDetails.Item[] specificMovieSearch = new MovieDetails.Item[1];
             if (!string.IsNullOrWhiteSpace(MovieIdentifier))
             {
@@ -27,9 +32,13 @@ namespace XMLProjectIS.Pages
                     if (currentMovie.Equals(MovieIdentifier.ToLower()))
                     {
                         specificMovieSearch[0] = searchMovie;
+
+                        if (specificMovieSearch[0] != null)
+                        { break; }
                     }
 
                 }
+
                 if (specificMovieSearch[0] == null)
                 {
                     var TVDetailsData = adapter.GetTVDetails();
@@ -40,6 +49,8 @@ namespace XMLProjectIS.Pages
                         if (currentTV.Equals(MovieIdentifier.ToLower()))
                         {
                             specificTVSearch[0] = searchTV;
+                            if (specificTVSearch[0] != null)
+                            { break; }
                         }
 
                     }
@@ -48,6 +59,9 @@ namespace XMLProjectIS.Pages
                 }
 
                 movieDetailsData = specificMovieSearch;
+
+
+                /*ViewData["PlayerData"] = playerData;*/
             }
             else
             {
@@ -65,5 +79,3 @@ namespace XMLProjectIS.Pages
         }
     }
 }
-
-            
